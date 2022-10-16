b1ea2781068b997c36930053c8d95a0b21b0c3a2
Luka Jeremic <ljeremic4019rn@raf.rs>
2022-10-16 19:03:29 +0200

    ruter napravljen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
+import {EntityExtractionComponent} from "./components/entity-extraction/entity-extraction.component";
+import {TextSimilarityComponent} from "./components/text-similarity/text-similarity.component";
+import {LanguageDetectionComponent} from "./components/language-detection/language-detection.component";
+import {SentimentAnalysisComponent} from "./components/sentiment-analysis/sentiment-analysis.component";
+import {TokenEntryComponent} from "./components/token-entry/token-entry.component";
+import {ApiHistoryComponent} from "./components/api-history/api-history.component";
 
 const routes: Routes = [
   {
     path: "",
-    component: HomeComponent
+    component: EntityExtractionComponent
   },
+  {
+    path: "textSimilarity",
+    component: TextSimilarityComponent
+  },
+  {
+    path: "languageDetection",
+    component: LanguageDetectionComponent
+  },
+  {
+    path: "sentimentAnalysis",
+    component: SentimentAnalysisComponent
+  },
+  {
+    path: "tokenEntry",
+    component: TokenEntryComponent
+  },
+  {
+    path: "apiHistory",
+    component: ApiHistoryComponent
+  },
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,24 @@ import {HttpClientModule} from "@angular/common/http";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {PasswordPipe} from "./pipes/password.pipe";
 import {AppComponent} from "./components/app/app.component";
+import {EntityExtractionComponent} from "./components/entity-extraction/entity-extraction.component";
+import {TextSimilarityComponent} from "./components/text-similarity/text-similarity.component";
+import {LanguageDetectionComponent} from "./components/language-detection/language-detection.component";
+import {SentimentAnalysisComponent} from "./components/sentiment-analysis/sentiment-analysis.component";
+import {TokenEntryComponent} from "./components/token-entry/token-entry.component";
+import {ApiHistoryComponent} from "./components/api-history/api-history.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    // PostListComponent,
-    // SinglePostComponent,
     PasswordPipe,
-    // ConfigurationComponent,
+    EntityExtractionComponent,
+    TextSimilarityComponent,
+    LanguageDetectionComponent,
+    SentimentAnalysisComponent,
+    TokenEntryComponent,
+    ApiHistoryComponent
   ],
 
     imports: [
